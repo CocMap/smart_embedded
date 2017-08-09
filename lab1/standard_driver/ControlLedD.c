1becc405a7959b6f1dc8b9ef628f17550a0e62d6
CocMap <CocMap@users.noreply.github.com>
2017-08-09 11:17:15 +0700

    lab1 - original file

---
+++ lab1/standard_driver/ControlLedD.c
@@ -0,0 +1,23 @@
+//------------------------------------------- main.c CODE STARTS ---------------------------------------------------------------------------
+// GPIO_LED : GPIO output to control an on-board red LED
+//
+// EVB : Nu-LB-NUC140
+// MCU : NUC140VE3CN
+// low-active output control by GPC12
+#include <stdio.h>
+#include "NUC100Series.h"
+#include "MCU_init.h"
+#include "SYS_init.h"
+int main(void)
+{
+ SYS_Init();      //initial setup 
+ GPIO_SetMode(PC, BIT12, GPIO_MODE_OUTPUT);
+ 
+ while(1) {
+   PC12 =0; // turn on LED, PORT C 12, led5
+   CLK_SysTickDelay(100000); // Delay
+   PC12 =1; // turn off LED
+   CLK_SysTickDelay(100000); // Delay
+  }
+}
+//------------------------------------------- main.c CODE ENDS ---------------------------------------------------------------------------
